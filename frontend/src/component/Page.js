0e8a693329c1d1441c84b6bf4b237d8d938732f0
Maduro29 <manhdungtran.vnuuet@gmail.com>
2023-12-03 19:57:46 +0700

    statistics design

--- frontend/src/component/Page.js
+++ frontend/src/component/Page.js
@@ -1,11 +1,18 @@
 import './Page.scss'
 import Home from '../pages/Home';
+import { Route, Routes } from 'react-router-dom'
+import Statistics from '../pages/Statistics';
+import Details from '../pages/Details';
 
 const Page = () => {
     return <>
         <div className='page'>
             <div className='page-box'>
-                <Home />
+                <Routes >
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/stat' element={<Statistics />}></Route>
+                    <Route path='/detail' element={<Details />}></Route>
+                </Routes>
             </div>
         </div>
     </>
--- frontend/src/component/Sidebar.js
+++ frontend/src/component/Sidebar.js
@@ -1,15 +1,41 @@
+import { useState } from 'react';
 import Logo from '../assets/images/logo.png';
 import './Sidebar.scss'
+import { Link, useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+
+    const navigate = useNavigate();
+
+    const [code, setCode] = useState('')
+
+    const handleView = (e) => {
+        if (e.keyCode === 13) {
+            navigate(`/detail?shipmentCode=${code}`)
+        }
+    }
+
+    const handleInputChange = (e) => {
+        setCode(e.target.value)
+    }
+
     return <>
         <div className='sidebar'>
             <div className='sidebar-box'>
                 <div className='sidebar-1'>
                     <img className='sidebar-logo' src={Logo}></img>
-                    <input className='sidebar-search' type='text'></input>
-                    <button className='sidebar-statistics sidebar-bottom'>Statistics</button>
-                    <button className='sidebar-statistics sidebar-bottom'>Statistics</button>
+                    <input className='sidebar-search' type='text' placeholder='Search by code' onKeyDown={(e) => handleView(e)}
+                        onChange={(e) => handleInputChange(e)}></input>
+                    <Link to={'/'}>
+                        <button className='sidebar-statistics sidebar-bottom'>
+                            Home
+                        </button>
+                    </Link>
+                    <Link to={'/stat'}>
+                        <button className='sidebar-statistics sidebar-bottom'>
+                            Statistics
+                        </button>
+                    </Link>
                 </div>
                 <div className='sidebar-2'>
                     <button className='sidebar-setting sidebar-top'>Settings</button>
