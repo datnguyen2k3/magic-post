8717a8f2feccbbff06ac7ea9cade7882134d09c4
Nguyễn Việt Hoàng (TTS KTCN) <tts_hoangnv@viettel-softwareservices.vn>
2023-12-06 22:26:34 +0700

    add entity

---
+++ backend/src/main/java/web/uet/backend/common/enums/Role.java
@@ -0,0 +1,13 @@
+package web.uet.backend.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Role {
+    EMPLOYEE,
+    POST_HEAD,
+    WAREHOUSE_HEAD,
+    CEO
+}
---
+++ backend/src/main/java/web/uet/backend/entity/business/Delivery.java
@@ -0,0 +1,79 @@
+package web.uet.backend.entity.business;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+import web.uet.backend.common.enums.ProductType;
+import web.uet.backend.entity.location.Commune;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(schema = "public", name = "delivery")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Delivery {
+
+    @Id
+    @Column(name = "delivery_id")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID deliveryId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "from_commune_id")
+    private Commune fromCommune;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "to_commune_id")
+    private Commune toCommune;
+
+    @Column(name = "from_address")
+    private String fromAddress;
+
+    @Column(name = "to_address")
+    private String toAddress;
+
+    @Column(name = "from_phone")
+    private String fromPhone;
+
+    @Column(name = "to_phone")
+    private String toPhone;
+
+    @Column(name = "from_name")
+    private String fromName;
+
+    @Column(name = "to_name")
+    private String toName;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "from_shop_id")
+    private Shop fromShop;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "to_shop_id")
+    private Shop toShop;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "product")
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private ProductType productType;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
