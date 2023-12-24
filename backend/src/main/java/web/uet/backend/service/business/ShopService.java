787770197e27990c34d77eb3c7dfc0b518baa21d
datnguyen2k3 <thanhdatmu2003@gmail.com>
2023-12-24 10:59:30 +0700

    update delivery number in shop

--- backend/src/main/java/web/uet/backend/service/business/ShopService.java
+++ backend/src/main/java/web/uet/backend/service/business/ShopService.java
@@ -1,7 +1,12 @@
 package web.uet.backend.service.business;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import web.uet.backend.entity.business.DeliveryStatus;
+import web.uet.backend.entity.business.Shop;
+import web.uet.backend.event.DeliveryStatusCreateEvent;
 import web.uet.backend.repository.business.jpa.ShopRepository;
 
 @Service
@@ -9,5 +14,40 @@ import web.uet.backend.repository.business.jpa.ShopRepository;
 public class ShopService {
 
   private final ShopRepository shopRepository;
+
+  @EventListener
+  @Transactional
+  public void updateDeliveryNumber(DeliveryStatusCreateEvent event) {
+    DeliveryStatus deliveryStatus = (DeliveryStatus) event.getSource();
+    Shop currentShop = deliveryStatus.getCurrentShop();
+
+    Integer currentDeliveryNumber = currentShop.getCurrentDeliveryNumber();
+    Integer comingDeliveryNumber = currentShop.getComingDeliveryNumber();
+    Integer goneDeliveryNumber = currentShop.getGoneDeliveryNumber();
+
+    switch (deliveryStatus.getStatusType()) {
+      case RECEIVED_FROM_CUSTOMER, SENT_TO_CUSTOMER_FAIL:
+        currentShop.setCurrentDeliveryNumber(currentDeliveryNumber + 1);
+        break;
+      case GONE_FROM_SHOP:
+        currentShop.setCurrentDeliveryNumber(currentDeliveryNumber - 1);
+        break;
+      case COMING_TO_SHOP:
+        currentShop.setComingDeliveryNumber(comingDeliveryNumber + 1);
+        break;
+      case RECEIVED_FROM_SHOP:
+        currentShop.setComingDeliveryNumber(comingDeliveryNumber - 1);
+        currentShop.setCurrentDeliveryNumber(currentDeliveryNumber + 1);
+        break;
+      case SHIPPING_TO_CUSTOMER:
+        currentShop.setCurrentDeliveryNumber(currentDeliveryNumber - 1);
+        currentShop.setGoneDeliveryNumber(goneDeliveryNumber + 1);
+        break;
+      case SENT_TO_CUSTOMER_SUCCESS:
+        break;
+    }
+
+    shopRepository.save(currentShop);
+  }
 
 }
